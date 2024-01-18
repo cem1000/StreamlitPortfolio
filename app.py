94777f653ee7a68366c5ab60502f5b25b4f2d12f
cem1000 <55397332+cem1000@users.noreply.github.com>
2024-01-18 21:22:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ col1, col2, col3 = st.columns(3)
 # App 1 - SQL Code Where Items App
 with col1:
     st.subheader('SQL Code Where Items App')
-    st.image(image_url, caption='SQL Where Clause Formatter App', use_column_width=True)
+    st.image(sql_app_image , caption='SQL Where Clause Formatter App', use_column_width=True)
     st.write('Streamlines SQL query building with efficient multi-value integration for where clause. If you are in a hurry and no time to format that long list of values in excel, dump them in here!')
     st.markdown('[Go to app](https://cem1000-sql-code-where-items-app-u1x6zr.streamlit.app/)')
     st.markdown('[Github URL](https://github.com/cem1000/sql_code_where_items)')
