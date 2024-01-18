959288f438bd10723db2499397722e81794483c4
cem1000 <55397332+cem1000@users.noreply.github.com>
2024-01-18 22:21:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,36 +15,36 @@ def gallery_page():
     # Create rows of columns for the apps
     col1, col2, col3 = st.columns(3)
     
-    # App 1 - SQL Code Where Items App
+    # App 1 
     
     with col1:
-        st.subheader('SQL Code Where Items App')
-        st.image(sql_app_image, caption='SQL Where Clause Formatter App', use_column_width=True)
-        st.write('Streamlines SQL query building with efficient multi-value integration for where clause. If you are in a hurry and no time to format that long list of values in excel, dump them in here!')
-        st.markdown('[Go to app](https://cem1000-sql-code-where-items-app-u1x6zr.streamlit.app/)')
-        st.markdown('[Github URL](https://github.com/cem1000/sql_code_where_items)')
-    
-    
-    # App 2 - Hotel Discovery Ireland
-    with col2:
-        st.subheader('Hotel Discovery Ireland')
-        st.image(hotel_app_image, caption='SQL Where Clause Formatter App', use_column_width=True)
-        st.write('Demonstrates folium map package usage and utilisation of latitude and longitude distance calculations. Grabs hotel information from https://app.hotelbeds.com/.')
-        st.markdown('[Go to app](https://hoteldiscoveryireland.streamlit.app/)')
-        st.markdown('[Github URL](https://github.com/cem1000/HotelDiscovery)')
-    
-    # App 3 - Dublin Bikes
-    with col3:
         st.subheader('Dublin Bikes')
         st.image(dublin_bike_image, caption='SQL Where Clause Formatter App', use_column_width=True)
         st.write('Enables Dublin bike users to efficiently plan their bike journey ahead of time using historical data. Data is derived from smart Dublin API providers.')
         st.markdown('[Go to app](https://dublinbikes.streamlit.app/)')
         st.markdown('[Github URL](https://github.com/cem1000/DublinBikeAvailability)')
     
+    
+    # App 2 
+    with col2:
+        st.subheader('SQL Code Where Items App')
+        st.image(sql_app_image, caption='SQL Where Clause Formatter App', use_column_width=True)
+        st.write('Streamlines SQL query building with efficient multi-value integration for where clause. If you are in a hurry and no time to format that long list of values in excel, dump them in here!')
+        st.markdown('[Go to app](https://cem1000-sql-code-where-items-app-u1x6zr.streamlit.app/)')
+        st.markdown('[Github URL](https://github.com/cem1000/sql_code_where_items)')
+    
+    # App 3
+    with col3:
+        st.subheader('Hotel Discovery Ireland')
+        st.image(hotel_app_image, caption='SQL Where Clause Formatter App', use_column_width=True)
+        st.write('Demonstrates folium map package usage and utilisation of latitude and longitude distance calculations. Grabs hotel information from https://app.hotelbeds.com/.')
+        st.markdown('[Go to app](https://hoteldiscoveryireland.streamlit.app/)')
+        st.markdown('[Github URL](https://github.com/cem1000/HotelDiscovery)')
+    
     # Start a new row for the next set of apps
     col4, col5, col6 = st.columns(3)
     
-    # App 4 - Clipnote
+    # App 4 
     with col4:
         st.subheader('Clipnote')
         st.image(clipnote_image, caption='SQL Where Clause Formatter App', use_column_width=True)
