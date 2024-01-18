ce672bed7d6a3a888b94e0629771ab94616df7b8
cem1000 <55397332+cem1000@users.noreply.github.com>
2024-01-18 21:24:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 clipnote_image = 'https://drive.google.com/uc?export=view&id=1ALwBvPU7qXRxtniOc885gcuYtsC93jdl'
 dublin_bike_image = 'https://drive.google.com/uc?export=view&id=176lKfvwFzusZM7EdNy5wPvSOM11d9kja'
 hotel_app_image = 'https://drive.google.com/uc?export=view&id=1kil09Cr5grElPBCOUJxIHEm9WIILpHwg'
-sql_app_image = 'https://drive.google.com/uc?export=view&id=1bqXocbT7Vnur9Gl9z778BpWM65nh8EB1'
+# sql_app_image = 'https://drive.google.com/uc?export=view&id=1bqXocbT7Vnur9Gl9z778BpWM65nh8EB1'
 
 
 # This should be the first Streamlit function called
@@ -16,15 +16,15 @@ st.title('My Streamlit Portfolio')
 col1, col2, col3 = st.columns(3)
 
 # App 1 - SQL Code Where Items App
+sql_app_image = 'https://drive.google.com/uc?export=view&id=1bqXocbT7Vnur9Gl9z778BpWM65nh8EB1'
+
 with col1:
     st.subheader('SQL Code Where Items App')
-    # st.image(sql_app_image , caption='SQL Where Clause Formatter App', use_column_width=True)
-
-    st.markdown(f'<img src="{sql_app_image}" alt="Image" style="width:100%;">', unsafe_allow_html=True)
-
+    st.image(sql_app_image, caption='SQL Where Clause Formatter App', use_column_width=True)
     st.write('Streamlines SQL query building with efficient multi-value integration for where clause. If you are in a hurry and no time to format that long list of values in excel, dump them in here!')
     st.markdown('[Go to app](https://cem1000-sql-code-where-items-app-u1x6zr.streamlit.app/)')
     st.markdown('[Github URL](https://github.com/cem1000/sql_code_where_items)')
+
 
 # App 2 - Hotel Discovery Ireland
 with col2:
