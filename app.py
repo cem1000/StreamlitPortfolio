3294d12f1c4b6cc9fc094afb57bd394168f6549f
cem1000 <55397332+cem1000@users.noreply.github.com>
2024-01-18 21:31:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ with col1:
 # App 2 - Hotel Discovery Ireland
 with col2:
     st.subheader('Hotel Discovery Ireland')
+    st.image(hotel_app_image, caption='SQL Where Clause Formatter App', use_column_width=True)
     st.write('Utilizes haversine formula and Folium maps for exploring nearby hotels. Demonstrates folium map package usage and creation of programming requiring functions in backend connected to a dashboard.')
     st.markdown('[Go to app](https://hoteldiscoveryireland.streamlit.app/)')
     st.markdown('[Github URL](https://github.com/cem1000/HotelDiscovery)')
@@ -35,6 +36,7 @@ with col2:
 # App 3 - Dublin Bikes
 with col3:
     st.subheader('Dublin Bikes')
+    st.image(dublin_bike_image, caption='SQL Where Clause Formatter App', use_column_width=True)
     st.write('Enables Dublin bike users to efficiently plan their bike journey ahead of time using historical data. Data is derived from smart Dublin API providers.')
     st.markdown('[Go to app](https://dublinbikes.streamlit.app/)')
     st.markdown('[Github URL](https://github.com/cem1000/DublinBikeAvailability)')
@@ -45,6 +47,7 @@ col4, col5, col6 = st.columns(3)
 # App 4 - Clipnote
 with col4:
     st.subheader('Clipnote')
+    st.image(clipnote_image, caption='SQL Where Clause Formatter App', use_column_width=True)
     st.write('An app for summarizing YouTube videos. It allows users to understand the content of long videos quickly by providing concise summaries.')
     st.markdown('[Go to app](https://clipnote.streamlit.app/)')
     # st.markdown('[Github URL](https://github.com/cem1000/yt_summarizer)')
